69ff21c4bc31a1fef1f07d5ab7c010ae158c19e9
8amjoker404 <8amjoker@gmail.com>
2026-09-02 08:48:17 -0700

    Update project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import AddExistingWalletPage from './pages/AddExistingWalletPage/AddExistingWalletPage'
+import LandingPage from './pages/LandingPage/LandingPage'
 import WalletDashboard from './pages/WalletDashboard/WalletDashboard'
 import './styles/app.css'
 import './styles/pages.css'
@@ -9,7 +10,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AddExistingWalletPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/add-existing-wallet" element={<AddExistingWalletPage />} />
         <Route path="/wallet" element={<WalletDashboard />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
